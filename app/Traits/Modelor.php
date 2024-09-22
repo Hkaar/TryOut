bfe2f56f67a3a4dc886780dbb85f7683dd228028
Hkaar <HkaarDev@gmail.com>
2024-09-22 21:42:20 +0800

    Added traits

---
+++ app/Traits/Modelor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait Modelor
+{
+    /**
+     * Updates the model based on if the data exists
+     *
+     * @param  array<string, mixed>  $data
+     * @param  array<string>  $except
+     */
+    public function updateModel(Model $model, array $data, array $except = []): void
+    {
+        $attributes = array_keys($model->getAttributes());
+
+        foreach ($data as $key => $value) {
+            if (! in_array($key, $except) && in_array($key, $attributes) && $value !== $model->$key) {
+                $model->$key = $value ?? $model->$key;
+            }
+        }
+    }
+}
