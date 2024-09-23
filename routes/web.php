53de2160e8ec1d21a8ad609679aa647b3ad9a301
Hkaar <HkaarDev@gmail.com>
2024-09-23 13:18:37 +0800

    Updated web routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(["namespace" => "App\Http\Controllers"], function () {
+    Route::get('/', 'HomeController@welcome')->name('/');
+    
+    Route::group(["middleware" => ["auth"]], function () {
+        Route::get('home', 'HomeController@index')->name('home');
+
+        Route::get('exams', 'ExamController@index')->name('exams.index');
+        Route::get('exams/{id}', 'ExamController@show')->name('exams.show');
+    });
+});
+
+Route::group(["namespace" => "App\Http\Controllers\Auth"], function () {
+    Route::group(["middleware" => ["guest"]], function () {
+        Route::post("login", "LoginController@login")->name('login.post');
+        Route::get('login', "LoginController@show")->name('login');
+    });
+
+    Route::group(["middleware" => ["auth"]], function() {
+        Route::get("logout", "LogoutController@perform")->name('logout');
+    });
+});
+
+Route::group(["namespace" => "App\Http\Controllers\Admin", "prefix" => "manage", "middleware" => ["can:access-dashboard"]], function () {
+    Route::get("home", "HomeController@index")->name("admin.home");
+
+    Route::resource('packets', 'PacketController')->names('admin.packets');
+    Route::resource('questions', 'QuestionController')->names('admin.questions');
+    Route::resource('subjects', 'SubjectController')->names('admin.subjects');
+
+    Route::group(["middleware" => ["can:admin"]], function () {
+        Route::resource('exams', 'ExamController')->names('admin.exams');
+        Route::resource('students', 'StudentController')->names('admin.students');
+        Route::resource('users', "UserController")->names('admin.users');
+        Route::resource('groups', 'GroupController')->names('admin.groups');
+    });
 });
