c9cedfe043cd6c140efa90c0420f69a74bcc0d25
Hkaar <HkaarDev@gmail.com>
2024-09-23 13:20:12 +0800

    Updated admin views

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
-@section('title', 'Akun - Dashboard')
+@section('title', 'Peserta - Dashboard')
 
 @section('content')
-  <x-dashboard-layout active="akun">
+  <x-dashboard-layout active="peserta">
     <x-elevated-card class="flex-1">
       hey
     </x-elevated-card>
--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -1,30 +1,29 @@
 @extends('layouts.app')
 
-@section('title', 'Mata Pelajaran - Dashboard')
+@section('title', 'Peserta - Dashboard')
 
 @php
-  $title = 'mata pelajaran';
+  $title = 'peserta';
 
   $columns = [
     ['width' => 5, 'name' => 'ID'], 
     'Nama', 
-    'Kode',
-    'Mata Pelajaran',
+    'Email',
     ['width' => 18, 'name' => 'Actions']
   ];
 
   $routes = [
-    'edit'
+    'create' => route('admin.students.create'),
   ];
 @endphp
 
 @section('content')
-  <x-dashboard-layout active="mapel">
+  <x-dashboard-layout active="peserta">
     <x-data-table :columns="$columns" :routes="$routes" :title="$title">
       <x-slot name="filters">
-        <form action="{{ route('admin.subjects.index') }}" method="get" class="flex flex-col gap-2 md:flex-row">
+        <form action="{{ route('admin.students.index') }}" method="get" class="flex flex-col gap-2 md:flex-row">
           <div class="relative flex-1">
-            <input type="search" name="search" class="form-control peer ps-11 shadow-sm" placeholder="Cari mapel ...">
+            <input type="search" name="search" class="form-control peer ps-11 shadow-sm" placeholder="Cari peserta ...">
 
             <div class="form-control-icon peer-disabled:pointer-events-none">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -46,30 +45,33 @@
         </form>
       </x-slot>
 
-      @foreach ($subjects as $i => $subject)
+      @foreach ($students as $i => $student)
         <tr
           class="dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700 odd:bg-white even:bg-gray-100 hover:bg-gray-100">
           <td class="dark:text-neutral-200 whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800">
             {{ $i + 1 }}
           </td>
           <td class="dark:text-neutral-200 whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-            {{ $subject->name }}
+            {{ $student->name }}
+          </td>
+          <td class="dark:text-neutral-200 whitespace-nowrap px-6 py-4 text-sm text-gray-800">
+            {{ $student->email }}
           </td>
           <td class="whitespace-nowrap flex gap-2 px-6 py-4 text-end text-sm font-medium">
-            <a href="{{ route('admin.subjects.edit', $subject->id) }}" class="btn bg-caution text-white flex items-center gap-2">
+            <a href="{{ route('admin.students.edit', $student->id) }}" class="btn bg-caution text-white flex items-center gap-2">
               <i class="material-symbols-outlined font-var-light">edit</i>
               Edit
             </a>
 
             <button type="button" class="btn bg-danger text-white flex items-center gap-2"
-              hx-confirm="mapel" hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'
-              hx-delete="{{ route('admin.subjects.destroy', $subject->id) }}" hx-target="closest tr" hx-swap="outerHTML"
+              hx-confirm="peserta" hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'
+              hx-delete="{{ route('admin.students.destroy', $student->id) }}" hx-target="closest tr" hx-swap="outerHTML"
               delete-confirmation>
               <i class="material-symbols-outlined font-var-light">delete</i>
               Hapus
             </button>
 
-            <a href="{{ route('admin.subjects.show', $subject->id) }}" class="btn bg-info text-white flex items-center gap-2">
+            <a href="{{ route('admin.students.show', $student->id) }}" class="btn bg-info text-white flex items-center gap-2">
               <i class="material-symbols-outlined font-var-light">info</i>
               Info
             </a>
@@ -79,9 +81,9 @@
 
       <x-slot name="footer">
         <div class="flex justify-between">
-          <x-paginate-links :links="$subjects"></x-paginate-links>
+          <x-paginate-links :links="$students"></x-paginate-links>
 
-          <x-paginate-counter :items="$subjects"></x-paginate-counter>
+          <x-paginate-counter :items="$students"></x-paginate-counter>
         </div>
       </x-slot>
     </x-data-table>
