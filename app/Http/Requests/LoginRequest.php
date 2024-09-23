903d89571ecd416d55ed9260f04bb7339c632551
Hkaar <HkaarDev@gmail.com>
2024-09-23 13:18:24 +0800

    Updated login request

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,18 +2,11 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -22,7 +15,18 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required'],
         ];
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @return array<string, string>
+     */
+    public function getCredentials(): array
+    {
+        return $this->only('email', 'password');
     }
 }
