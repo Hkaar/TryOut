221d93afc2ae9f19705b8cd778a1c3530f4d27d2
Hkaar <HkaarDev@gmail.com>
2024-09-29 07:49:04 +0800

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "manage", "namespace" => "App\Http\Controllers\API"], function () {
+    Route::group([], function () {
+        Route::get("pertanyaan", "QuestionController@index")->name("api.admin.questions.index");
+        Route::delete("pertanyaan", 'QuestionController@destroy')->name('api.admin.questions.destroy');
+
+        Route::get('ujian/statistics', 'ExamController@getExamStatistics')->name('api.admin.exams.stats');
+    });
 });
