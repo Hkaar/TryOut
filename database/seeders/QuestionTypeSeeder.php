6b002867595361cbf9cc53748be1146f2212c988
Hkaar <HkaarDev@gmail.com>
2024-09-29 07:48:42 +0800

    Updated seeders

---
+++ database/seeders/QuestionTypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\QuestionType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuestionTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = ['multiple_choice', 'essay'];
+
+        foreach ($types as $type) {
+            if (! QuestionType::StrictByName($type)->first()) {
+                QuestionType::create([
+                    'name' => $type,
+                ]);
+            }
+        }
+    }
+}
