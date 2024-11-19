883ea5449588c67ca39a639abc44ff306f666398
Hkaar <HkaarDev@gmail.com>
2024-11-19 10:08:23 +0800

    Updated show exam view

--- resources/views/exams/show.blade.php
+++ resources/views/exams/show.blade.php
@@ -16,8 +16,9 @@
           <x-card class="shadow-lg">
             <x-slot name="header">
               <div id="questionHeader">
-                <div class="rounded-t-lg bg-tertiary px-4 py-3 text-white line-clamp-1 flex items-center justify-between gap-2">
-                  <h3 class="text-xl font-bold line-clamp-1">
+                <div
+                  class="line-clamp-1 flex items-center justify-between gap-2 rounded-t-lg bg-tertiary px-4 py-3 text-white">
+                  <h3 class="line-clamp-1 text-xl font-bold">
                     Soal 1
                   </h3>
 
@@ -25,15 +26,14 @@
                     <input type="checkbox" name="not_sure"
                       class="dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800 mt-0.5 shrink-0 rounded border-gray-200 text-blue-600 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50"
                       id="hs-default-checkbox">
-                    <label for="hs-default-checkbox"
-                      class="ms-3 text-sm">Ragu-ragu</label>
+                    <label for="hs-default-checkbox" class="ms-3 text-sm">Ragu-ragu</label>
                   </div>
                 </div>
               </div>
             </x-slot>
 
             <form id="questionContainer">
-              <p class="text-xl font-medium mb-3">
+              <p class="mb-3 text-xl font-medium">
                 {{ $question->question->content }}
               </p>
 
@@ -43,7 +43,8 @@
                     <div class="flex">
                       <input type="radio" name="answer"
                         class="dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800 mt-0.5 shrink-0 rounded-full border-gray-200 text-blue-600 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50"
-                        id="choice-{{ $i }}" value="{{ $choice->content }}" {{ $question->answer === $choice->content ? 'checked' : '' }}>
+                        id="choice-{{ $i }}" value="{{ $choice->content }}"
+                        {{ $question->answer === $choice->content ? 'checked' : '' }}>
                       <label for="choice-{{ $i }}"
                         class="dark:text-neutral-400 ms-2 text-sm text-gray-500">{{ $choice->content }}</label>
                     </div>
@@ -60,18 +61,22 @@
 
             <x-slot name="footer">
               <div class="flex items-center gap-2 rounded-b-lg border-t border-gray-200 px-4 py-3">
-                <x-button type="button" id="previousQuestion" class="w-fit border-danger text-danger hover:bg-danger hover:text-white">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                <x-button type="button" id="previousQuestion"
+                  class="w-fit border-danger text-danger hover:bg-danger hover:text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                   </svg>
 
                   Sebelumnya
                 </x-button>
 
-                <x-button type="button" id="nextQuestion" class="w-fit border-success text-success hover:bg-success hover:text-white">
+                <x-button type="button" id="nextQuestion"
+                  class="w-fit border-success text-success hover:bg-success hover:text-white">
                   Selanjutnya
 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                   </svg>
                 </x-button>
@@ -81,17 +86,19 @@
         </div>
 
         <div class="flex w-full flex-col gap-4 md:col-span-1">
-          <div
-            class="flex h-3/4 flex-col gap-6 overflow-y-auto rounded-md border border-t-2 border-t-primary px-6 py-4 shadow-md">
-            <div class="flex items-center justify-between gap-2">
-              <h6 class="text-xl font-semibold">
-                Daftar Pertanyaan
-              </h6>
+          <x-card class="shadow-lg min-h-96">
+            <x-slot name="header">
+              <div
+                class="line-clamp-1 flex items-center justify-between gap-2 rounded-t-lg bg-tertiary px-4 py-3 text-white">
+                <h3 class="text-xl font-semibold">
+                  Daftar Pertanyaan
+                </h3>
 
-              <span class="font-semibold text-gray-400" id="examTimer">
-                00:00:00
-              </span>
-            </div>
+                <span class="font-semibold" id="examTimer">
+                  00:00:00
+                </span>
+              </div>
+            </x-slot>
 
             <div class="flex max-w-full flex-wrap gap-3">
               @foreach ($questions as $i => $item)
@@ -102,17 +109,23 @@
               @endforeach
             </div>
 
-            <button type="button" id="finishExam"
-              class="btn mt-auto flex w-full items-center justify-center gap-2 bg-danger px-4 py-3 text-white duration-150 ease-in-out hover:scale-105 hover:border-danger hover:bg-transparent hover:text-black active:scale-95 active:opacity-50 disabled:opacity-40 disabled:hover:scale-100 disabled:active:scale-100">
-              <i class="material-symbols-outlined font-var-light">playlist_add_check</i>
-              Akhiri Ujian
-            </button>
-          </div>
+            <x-slot name="footer">
+              <div class="flex items-center gap-2 rounded-b-lg border-t border-gray-200 px-4 py-3">
+                <x-button type="button" id="finishExam" class="flex-1 bg-danger text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                  </svg>                  
+                  Akhiri Ujian
+                </x-button>
+              </div>
+            </x-slot>
+          </x-card>
         </div>
       </div>
     </div>
+  </div>
 
-    <x-footer />
+  <x-footer />
   </div>
 @endsection
 
