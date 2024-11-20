29ce849dc98aa5059d4599e546005288b8182d8c
Hkaar <HkaarDev@gmail.com>
2024-11-21 00:04:23 +0800

    Used new plugin system to prevent unnecesary errors being logged

--- resources/js/utils/plugin.js
+++ resources/js/utils/plugin.js
@@ -1,10 +1,10 @@
 /**
  * Runs the setup function if a plugin is present in the page
  * 
- * @param {string[]} plugins
- * @param {CallableFunction} setupPlugin 
+ * @param {string[]} plugins - The list of plugins to be checked from the `<meta name="plugins">` tag
+ * @param {CallableFunction} setupPlugin - The setup function for the plugin
  */
-export function pluginRun(plugins, setupPlugin) {
+export function runPlugin(plugins, setupPlugin) {
     const pluginsMeta = document.querySelector(`meta[name="plugins"]`);
 
     if (!(pluginsMeta instanceof HTMLMetaElement)) {
@@ -12,7 +12,7 @@ export function pluginRun(plugins, setupPlugin) {
         return;
     }
 
-    const loadedPlugins = pluginsMeta.content.split(' ');
+    const loadedPlugins = pluginsMeta.content.replace(' ', '').split('|');
     const loaded = plugins.every(plugin => loadedPlugins.includes(plugin))
 
     if (loaded) {
@@ -20,5 +20,5 @@ export function pluginRun(plugins, setupPlugin) {
         return;
     }
 
-    console.info(`Plugin not loaded due to missing full set of ${plugins}!`);
+    console.debug(`Plugin not loaded due to missing full set of ${plugins}!`);
 }
