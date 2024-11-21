1f65530001430af43f0141639d1e805b43254770
Hkaar <HkaarDev@gmail.com>
2024-11-21 23:50:54 +0800

    fixed types

--- resources/types/app.d.ts
+++ resources/types/app.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="./questions.d.ts" />
+/// <reference types="./chart.d.ts" />
+/// <reference types="./admin.d.ts" />
+
 declare var examResult: number;
 declare var csrf: string;
 
