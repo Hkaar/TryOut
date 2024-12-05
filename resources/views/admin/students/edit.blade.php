da7c1c8a00b6f17314eed9ceb07e3f78a4890b5b
Hkaar <HkaarDev@gmail.com>
2024-12-06 00:30:55 +0800

    Added plugin of image-preview to some views

--- resources/views/admin/students/edit.blade.php
+++ resources/views/admin/students/edit.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Peserta - Dashboard')
 
+@section('meta')
+  <meta name="plugins" content="image-preview">
+@endsection
+
 @section('content')
   <x-dashboard-layout active="peserta">
     <div class="grid flex-1 grid-cols-1 gap-y-5 lg:grid-cols-2">
