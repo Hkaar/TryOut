f5aeedb2639f45b3ce3b7ef5c56de65ac3193d2f
Hkaar <HkaarDev@gmail.com>
2024-12-06 20:07:09 +0800

    Added seo specific meta tags to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,21 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title')</title>
+
+  <meta name="author" content="Dimensi Pelajar">
+  <meta name="description" content="Website ujian online untuk Dimensi Pelajar">
+  <meta name="keywords" content="exam, online, online exam, ujian, ujian online, dimensi pelajar">
+  <meta name="language" content="id">
+
+  <meta property="og:title" content="Dimensi Pelajar">
+  <meta property="og:description" content="Website ujian online untuk Dimensi Pelajar">
+  <meta property="og:image" content="https://github.com/Hkaar/TryOut/blob/dev/.github/images/cover.png?raw=true">
+  <meta property="og:url" content="https://dimensipelajar.com">
+  <meta property="og:type" content="website"> 
 
   @yield('meta')
+
+  <title>@yield('title')</title>
 
   @vite(['resources/css/app.css'])
   @stack('css')
