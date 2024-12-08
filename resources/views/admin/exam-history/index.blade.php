93981af830954b28ad9a1fb02e47e179a9d0dbdd
Hkaar <HkaarDev@gmail.com>
2024-12-08 12:07:01 +0800

    Updated index admin pages

--- resources/views/admin/exam-history/index.blade.php
+++ resources/views/admin/exam-history/index.blade.php
@@ -27,7 +27,8 @@
             <div class="relative">
               <input type="text"
                 class="peer block w-full rounded-lg border-gray-200 px-3 py-2 ps-11 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-transparent dark:bg-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                name="search" placeholder="Cari sebuah riwayat ujian" value="{{ request()->has('search') ? request()->input('search') : '' }}">
+                name="search" placeholder="Cari sebuah riwayat ujian"
+                value="{{ request()->has('search') ? request()->input('search') : '' }}">
               <div
                 class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-4 peer-disabled:pointer-events-none peer-disabled:opacity-50">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -44,21 +45,28 @@
             @enderror
           </div>
 
-          <select class="py-2 px-3 pe-9 block w-32 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" name="group">
+          <select
+            class="block w-32 rounded-lg border-gray-200 px-3 py-2 pe-9 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="group">
             <option selected disabled>Pilih group</option>
             <option value="all" {{ request()->input('group') === 'all' ? 'selected' : '' }}>Semua group</option>
 
             @foreach ($groups as $group)
-              <option value="{{ $group->id }}" {{ $group->id === ((int) request()->input('group')) ? 'selected' : '' }}>{{ ucfirst( $group->name ) }}</option>
+              <option value="{{ $group->id }}"
+                {{ $group->id === ((int) request()->input('group')) ? 'selected' : '' }}>{{ ucfirst($group->name) }}
+              </option>
             @endforeach
           </select>
 
-          <select class="py-2 px-3 pe-9 block w-32 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" name="exam">
+          <select
+            class="block w-32 rounded-lg border-gray-200 px-3 py-2 pe-9 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="exam">
             <option selected disabled>Pilih ujian</option>
             <option value="all" {{ request()->input('exam') === 'all' ? 'selected' : '' }}>Semua ujian</option>
 
             @foreach ($exams as $exam)
-              <option value="{{ $exam->id }}" {{ $exam->id === ((int) request()->input('exam')) ? 'selected' : '' }}>{{ ucfirst( $exam->name ) }}</option>
+              <option value="{{ $exam->id }}" {{ $exam->id === ((int) request()->input('exam')) ? 'selected' : '' }}>
+                {{ ucfirst($exam->name) }}</option>
             @endforeach
           </select>
 
@@ -73,8 +81,7 @@
               Terapkan
             </x-button>
 
-            <x-link-button :to="route('admin.exam-history.download', request()->query())"
-              class="bg-info text-white hover:rounded-none active:rounded-none">
+            <x-link-button :to="route('admin.exam-history.download', request()->query())" class="bg-info text-white hover:rounded-none active:rounded-none">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -93,13 +100,13 @@
         <tr
           class="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700">
           <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200">
-            {{ $i + 1 }}
+            {{ ($results->currentPage() - 1) * $results->perPage() + $i + 1 }}
           </td>
           <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800 dark:text-neutral-200">
             {{ $result->user->name }}
           </td>
-          <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800 dark:text-neutral-200">
-            {{ $result->exam->name }}
+          <td class="max-w-72 px-6 py-4 text-sm text-gray-800 dark:text-neutral-200">
+            {!! nl2br(e(strip_tags($result->exam->name))) !!}
           </td>
           <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800 dark:text-neutral-200">
             {{ Carbon\Carbon::parse($result->start_date)->locale('id')->translatedFormat('l, j F Y H:i:s T') }}
@@ -109,16 +116,16 @@
           </td>
           <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800 dark:text-neutral-200">
             @if ($result->finished)
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-6 fill-success">
+                <path fill-rule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  clip-rule="evenodd" />
               </svg>
             @else
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-6 fill-danger">
+                <path fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+                  clip-rule="evenodd" />
               </svg>
             @endif
           </td>
