1cb18d3d21dd52cb0c0c5a0d6e6b217efac62da2
Hkaar <HkaarDev@gmail.com>
2024-12-09 18:32:38 +0800

    Updated exam js components

--- resources/js/components/exams/ExamTimer.js
+++ resources/js/components/exams/ExamTimer.js
@@ -5,7 +5,7 @@
  */
 export default function ExamTimer() {
     const root = document.createElement("span");
-    root.className = "font-semibold px-2 py-1 rounded-lg bg-gray-100 text-black w-20 text-center";
+    root.className = "font-semibold px-3 py-2 lg:px-4 lg:py-3 rounded-full bg-primary text-white w-28 lg:w-32 text-center";
     root.id = "examTimer";
     root.textContent = "00:00:00";
 
